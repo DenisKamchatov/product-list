61de7f8c346eb829abb62eb8ffa742f6e969ccc7
Denis Kamchatov <kamcatovdenis@gmail.com>
2022-08-01 16:07:10 +0600

    main form without validation

---
+++ components/FormCreateItem.vue
@@ -0,0 +1,79 @@
+<template>
+  <form class="form" name="form-create-item">
+    <div class="form__label">
+      <label for="name">Наименование товара</label>
+      <div class="form__label-dot" />
+    </div>
+    <input type="text" name="name" placeholder="Введите наименование товара" required>
+
+    <label class="form__label" for="description">Описание товара</label>
+    <textarea id="form-description" name="description" placeholder="Введите описание товара" cols="30" rows="5" />
+
+    <div class="form__label">
+      <label for="image-link">Ссылка на изображение товара</label>
+      <div class="form__label-dot" />
+    </div>
+    <input type="text" name="image-link" placeholder="Введите ссылку" required>
+
+    <div class="form__label">
+      <label for="image-link">Цена товара</label>
+      <div class="form__label-dot" />
+    </div>
+    <input type="number" name="image-link" placeholder="Введите цену" required>
+
+    <ButtonForm form="form-create-item" text="Добавить товар" />
+  </form>
+</template>
+
+<script>
+import ButtonForm from './UI/ButtonForm.vue'
+export default {
+  name: 'FormCreateItem',
+  components: { ButtonForm }
+}
+</script>
+
+<style lang="scss">
+  @import "~assets/scss/base";
+  .form {
+    display: flex;
+    flex-direction: column;
+    background-color: $white;
+    box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04), 0px 6px 10px rgba(0, 0, 0, 0.02);
+    padding: 25px 20px;
+    max-width: 450px;
+
+    label {
+      font-size: 10px;
+      font-weight: 400;
+      color: $text-label;
+    }
+    &__label {
+      display: flex;
+    }
+    &__label:not(:first-child) {
+      margin: 20px 0 0 0;
+    }
+    &__label-dot {
+      background-color: $required-dot;
+      width: 4px;
+      height: 4px;
+      border-radius: 50%;
+      margin: 0 0 0 3px;
+    }
+
+    input, textarea {
+      background-color: $white;
+      box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+      padding: 10px;
+      margin: 7px 0 0 0;
+    }
+
+    input::placeholder, textarea::placeholder {
+      font-size: 14px;
+      font-weight: 400;
+      color: $placeholder;
+    }
+  }
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,43 @@
 <template>
-  <Tutorial />
+  <div class="container">
+    <header class="header">
+      <h1 class="header__title">
+        Добавление товара
+      </h1>
+    </header>
+    <main class="main">
+      <FormCreateItem />
+      <!-- <div class="main__products products">
+        <ProductItem name="123" description="456" image="dasd" />
+      </div> -->
+    </main>
+    <nuxt />
+  </div>
 </template>
 
 <script>
+import FormCreateItem from '../components/FormCreateItem.vue'
+// import ProductItem from '~/components/ProductItem.vue'
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: { FormCreateItem },
+  data () {
+    return {
+    }
+  }
 }
+/**
+ * ff - Source Sans Pro
+ * h1 - fw600, fz28px
+ * h2 - fw600, fz24px
+ * h3 - fw600, fz20px
+ * p - fw400, fz16px
+ * label - fw400, fz10px
+ * ph - fw400, fz12
+ * button - fw600, fz12
+ */
 </script>
+
+<style lang="scss">
+  @import "~assets/scss/base";
+</style>
